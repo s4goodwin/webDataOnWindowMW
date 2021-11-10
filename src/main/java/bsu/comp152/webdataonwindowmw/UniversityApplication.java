13e23acb25fccff5f579c8b62d3f2cb06b68b5af
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-11-10 15:03:14 -0500

    11/10 after class

--- src/main/java/bsu/comp152/webdataonwindowmw/UniversityApplication.java
+++ src/main/java/bsu/comp152/webdataonwindowmw/UniversityApplication.java
@@ -7,11 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class UniversityApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(UniversityApplication.class.getResource("Main.fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 600, 400);
+        }
+        catch (IOException e) {
+            System.out.println("error loading FXML file or exception in controller class");
+        }
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/bsu/comp152/webdataonwindowmw/UniversityController.java
+++ src/main/java/bsu/comp152/webdataonwindowmw/UniversityController.java
@@ -3,7 +3,7 @@ package bsu.comp152.webdataonwindowmw;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class UniversityController {
     @FXML
     private Label welcomeText;
 
